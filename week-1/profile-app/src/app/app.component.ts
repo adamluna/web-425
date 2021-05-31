8cfd63cf80add7b8a1918489a04bf27318dbeecf
Adam Luna <adam.luna@outlook.com>
2021-05-30 20:20:49 -0500

    Add Assignment 1.5

---
+++ week-1/profile-app/src/app/app.component.ts
@@ -0,0 +1,19 @@
+/*
+============================================
+; Title:  Assignment 1.5
+; Author: Adam Luna
+; Date: 30 May 2021
+; Description: Components
+;===========================================
+*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  assignment : string = 'Assignment 1.5 - Components';
+}
