e41baaa4ada88d093712b4f6e6571034d855d986
Adam Luna <adam.luna@outlook.com>
2021-06-06 17:41:12 -0500

    Add Exercise 2.2

---
+++ week-2/my-app/src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+/*
+========================================
+; Title: Assignment 2.2
+; Author: Adam Luna
+; Date: 6 June 2021
+; Desctiption: Navigation
+========================================
+*/
+
+// Import modules
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+// Route paths
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
