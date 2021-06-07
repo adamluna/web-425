ee5397904d26f6dbc3873655b4ec9120e5bd6ea6
Adam Luna <adam.luna@outlook.com>
2021-06-06 19:32:44 -0500

    Add Assignment 2.4

---
+++ week-2/composer-app/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ComposerListComponent } from './composer-list/composer-list.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  { path: 'home', component: ComposerListComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
