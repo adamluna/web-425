8836fe6ca0643a5c8e32ef02699e73d04977d3a6
Adam Luna <adam.luna@outlook.com>
2021-06-27 22:05:51 -0500

    Add Assignment 5.4

---
+++ week-5/in-n-out-booksp3/src/app/book.interface.ts
@@ -0,0 +1,17 @@
+/*
+============================================
+; Title:  Exercise 5.2
+; Author: Adam Luna
+; Date: 27 June 2021
+; Description: Book interface file
+;===========================================
+*/
+
+// Create interface named IBook
+export interface IBook {
+    isbn: string;
+    title: string;
+    authors: Array<string>;
+    description: string;
+    numOfPages: number;
+  }
