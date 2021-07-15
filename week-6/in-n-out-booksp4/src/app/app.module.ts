8797bcc597d34b80c34022ca81fab53c1d91583d
Adam Luna <adam.luna@outlook.com>
2021-07-15 01:04:15 -0500

    Fix Event Emitter error

--- week-6/in-n-out-booksp4/src/app/app.module.ts
+++ week-6/in-n-out-booksp4/src/app/app.module.ts
@@ -17,7 +17,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { BookListComponent } from './book-list/book-list.component';
-
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
@@ -48,13 +47,16 @@ import { MatInputModule } from '@angular/material/input';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule,
     MatCardModule,
+    MatButtonModule,
     MatListModule,
     FlexLayoutModule,
     MatIconModule,
     MatTableModule,
     MatDialogModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
